d7419436d2b527a3223e1e906a1c2e1308b97d7e
Nelson Neves <nelson.s.neves@gmail.com>
2016-09-01 17:47:10 +0100

    add new influxdb connector + babl-module-request dashboard

--- grafana_deploy_custom_public.sh
+++ grafana_deploy_custom_public.sh
@@ -3,4 +3,5 @@
 npm install --prefix $GOPATH/src/github.com/grafana/grafana/
 npm run build --prefix $GOPATH/src/github.com/grafana/grafana/
 
+rm -rf ./public
 cp -R $GOPATH/src/github.com/grafana/grafana/public_gen ./public
--- grafana_docker_build_run.sh
+++ grafana_docker_build_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GRAFANA_VERSION="v3.0.5"
+GRAFANA_VERSION="v3.0.6"
 
 docker build -t nneves-grafana:$GRAFANA_VERSION .
 
